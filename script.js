da52a7be63b3b3f5bc769eb438b6459ff50a14f9
Rajath A R <92567686+rajathar4@users.noreply.github.com>
2021-12-26 00:02:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Get the modal
+
+
+function clearName () {
+    console.log("clicked") 
+    document.getElementById("id05").value = ''
+    document.getElementById("Name").value = '' 
+  }
+
+
+
+  function enteredValue () {
+      return 10 ;
+  }
+
+
+
+ 
